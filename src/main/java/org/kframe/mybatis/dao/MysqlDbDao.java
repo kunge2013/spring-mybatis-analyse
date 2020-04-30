b7c666421016d5fc9ce4835570ac41e85a52905f
kunge2013 <543976974@qq.com>
2020-04-30 22:27:49 +0800

    1

--- src/main/java/org/kframe/mybatis/dao/MysqlDbDao.java
+++ src/main/java/org/kframe/mybatis/dao/MysqlDbDao.java
@@ -3,10 +3,8 @@ package org.kframe.mybatis.dao;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
-@Mapper
 public interface MysqlDbDao {
 
 	@Select("select host, user from db")
