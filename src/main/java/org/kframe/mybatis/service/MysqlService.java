66bdc7a4ba329dc2b5c2299421e59a4f395d6055
kunge2013 <543976974@qq.com>
2020-04-30 22:27:00 +0800

    1

--- src/main/java/org/kframe/mybatis/service/MysqlService.java
+++ src/main/java/org/kframe/mybatis/service/MysqlService.java
@@ -3,6 +3,7 @@ package org.kframe.mybatis.service;
 import org.kframe.mybatis.dao.MysqlDbDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class MysqlService implements IMysqlService {
@@ -10,6 +11,7 @@ public class MysqlService implements IMysqlService {
 	@Autowired
 	private MysqlDbDao dao;
 
+	@Transactional
 	@Override
 	public Object list() {
 		// TODO Auto-generated method stub
