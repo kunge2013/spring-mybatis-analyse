309293ed0adb18b8718573b194a2cf39c73bb38a
fangkun <panzhangyi@squrab.com>
2020-04-30 17:08:54 +0800

    1

--- src/main/java/org/kframe/springioc/annotations/ServiceFactoryBeanTest.java
+++
@@ -1,8 +0,0 @@
-package org.kframe.springioc.annotations;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class ServiceFactoryBeanTest {
-
-}
--- src/main/java/org/kframe/springioc/service/IDbService.java
+++
@@ -1,5 +0,0 @@
-package org.kframe.springioc.service;
-
-public interface IDbService {
-	 public void save();
-}
