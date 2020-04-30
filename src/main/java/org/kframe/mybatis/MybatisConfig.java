1ed49ea71ffafb6537e286a8445a95f7200213bd
fangkun <panzhangyi@squrab.com>
2020-04-30 15:50:52 +0800

    1

--- src/main/java/org/kframe/mybatis/MybatisConfig.java
+++ src/main/java/org/kframe/mybatis/MybatisConfig.java
@@ -28,7 +28,7 @@ public class MybatisConfig {
 	private DataSource dataSource() {
 		SimpleDriverDataSource datasource = new SimpleDriverDataSource();
 		datasource.setDriverClass(com.mysql.jdbc.Driver.class);
-		datasource.setUrl("jdbc:mysql://localhost:3306/mysql?useUnicode=true&characterEncoding=utf8&useSSL=false&serverTimezone=Hongkong");
+		datasource.setUrl("jdbc:mysql://localhost:3306/mysql?useUnicode=true&characterEncoding=utf8&useSSL=false&serverTimezone=Asia/Shanghai");
 		datasource.setUsername("fk");
 		datasource.setPassword("123456");
 		
